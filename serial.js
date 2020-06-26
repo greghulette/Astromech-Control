37776eaab0e9b7d4d966a6c24dcd793599ac4eb7
greghulette <greg.hulette@gmail.com>
2020-06-26 15:40:52 -0400

    made the serialport work without rebooting everytime I send a command.  It reboots the first time only.

--- serial.js
+++ serial.js
@@ -1,53 +1,67 @@
 const SerialPort = require('serialport');
 const port = new SerialPort('/dev/cu.usbmodem14101', {
     baudRate: 57600,
-    autoOpen: false, });
+    autoOpen: false,
+    });
+
+function serialSend(lastLDPcommands) {
+
+  let command = lastLDPcommands + '\r';
+
+  if (port.IsOpen) {
+    console.log('Port is already open 123');
+    port.set({ options: dts = false });
+
+    // port.close(function (err) {
+    //    console.log('port closed', err);
+    //  });
+    // sleep(2000).then(() => { port.write(command); });
+  } else {
+    port.open(function (err) {
+      if (err) {
+
+        // port.set({ options: dts = false });
+        port.write(command);
+        return console.log('port opened already but sending the command of ' + command);
+      };
+      // port.set({ options: dts = false });
+      console.log('serial command says: ' + command);
+      sleep(2000).then(() => { port.write(command); });
+      // port.write(command);
+      // Because there's no callback to write, write errors will be emitted on the port:
+      // port.write('L123\n');
+    });
+    // port.close(function (err) {
+    //    console.log('port closed', err);
+    //  });
+  };
+
+
+};
+
+
+// Original function
 
 // function serialSend(lastLDPcommands) {
-//   let command = lastLDPcommands + '\r';
-//   if (port.IsOpen) {
-//     console.log('Port is already open 123');
+
+
+
+//   port.open(function (err) {
+//       if (err) {
+//         return console.log('Error opening port: ', err.message);
+//       }
+//       port.set({ options: dtr = false });
+//       var command = lastLDPcommands + '\r';
+//       console.log('serial command says: ' + command);
+//       sleep(2000).then(() => { port.write(command); });
+//       // Because there's no callback to write, write errors will be emitted on the port:
+//       // port.write('L123\n');
+//     })
 //     port.close(function (err) {
 //        console.log('port closed', err);
 //      });
-//     // sleep(2000).then(() => { port.write(command); });
-//   } else {
-//     port.open(function (err) {
-//       if (err) {
-//         return console.log('Error opening port: 1', err.message);
-//       };
 //
-//       console.log('serial command says: ' + command);
-//       sleep(2000).then(() => { port.write(command); });
-//
-//       // Because there's no callback to write, write errors will be emitted on the port:
-//       // port.write('L123\n');
-//     });
-//   };
-//
-//   port.close(function (err) {
-//      console.log('port closed', err);
-//    });
 // };
-
-
-//Original function
-function serialSend(lastLDPcommands){
-  port.close(function (err) {
-     console.log('port closed', err);
-   });
-   port.open(function (err) {
-      if (err) {
-        return console.log('Error opening port: ', err.message)
-      }
-      var command = lastLDPcommands + '\r';
-      console.log('serial command says: ' + command);
-      sleep(2000).then(() => { port.write(command); });
-      // Because there's no callback to write, write errors will be emitted on the port:
-      // port.write('L123\n');
-    })
-
-};
 
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
