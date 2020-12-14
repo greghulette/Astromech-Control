0b0d0bf079c9a9eb8c2e742e13ce1f70299a9041
greghulette <greg.hulette@gmail.com>
2020-12-14 15:43:23 -0500

    working on i2c commands

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ io.on('connection', function (socket) {
     var cbi = data.icommandstring;
     // sleep(500).then(() => { serial(ldp, coin, vu, maint, serialcommand, dp, cbi); });
 
-    sleep(500).then(() => { i2c(ldp, coin, vu, maint, serialcommand, dp, cbi); });
-// i2CSend(ldp, coin, vu, maint, serialcommand, dp, cbi);
+    // sleep(500).then(() => { i2c(ldp, coin, vu, maint, serialcommand, dp, cbi); });
+   i2c(ldp, coin, vu, maint, serialcommand, dp, cbi);
 
     // sleep(500).then(() => { serial(coin); });
 
