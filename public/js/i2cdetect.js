c26b7330f2d6b8a366c73ef6fbeb678cf6b23eef
greghulette <greg.hulette@gmail.com>
2021-02-02 16:30:26 -0500

    s

--- public/js/i2cdetect.js
+++ public/js/i2cdetect.js
@@ -12,10 +12,10 @@ const scan = (first, last) => {
   // fs.writeSync(0, '     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f');
 
   for (let addr = 0; addr <= 127; addr += 1) {
-    if (addr % 16 === 0) {
-      fs.writeSync(0, '\n' + (addr === 0 ? '0' : ''));
-      fs.writeSync(0, addr.toString(16) + ':');
-    }
+    // if (addr % 16 === 0) {
+    //   fs.writeSync(0, '\n' + (addr === 0 ? '0' : ''));
+    //   fs.writeSync(0, addr.toString(16) + ':');
+    // }
 
     if (addr < first || addr > last) {
       fs.writeSync(0, '   ');
