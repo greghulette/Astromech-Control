9356d68a5615d8733087457c8417bf1fe2cbdacc
greghulette <greg.hulette@gmail.com>
2021-02-03 09:23:06 -0500

    update i2c

--- public/js/i2cdetect.js
+++ public/js/i2cdetect.js
@@ -11,22 +11,14 @@ const i2devices = [];
 const EBUSY = 16; /* Device or resource busy */
 
 const scan = (first, last) => {
-  // fs.writeSync(0, '     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f');
 
   for (let addr = 0; addr <= 127; addr += 1) {
-    // if (addr % 16 === 0) {
-    //   fs.writeSync(0, '\n' + (addr === 0 ? '0' : ''));
-    //   fs.writeSync(0, addr.toString(16) + ':');
-    // }
 
     if (addr < first || addr > last) {
-      // fs.writeSync(0, '   ');
     } else {
       try {
         i2c1.receiveByteSync(addr);
-        // fs.writeSync(0, ' ' + addr.toString(16)); // device found, print addr
         i2devices.push(addr);
-
       } catch (e) {
         if (e.errno === EBUSY) {
           fs.writeSync(0, ' UU');
