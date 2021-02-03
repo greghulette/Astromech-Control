fb5d9a9d0ebb9bcd15407572d373f408e8b9453d
greghulette <greg.hulette@gmail.com>
2021-02-03 13:28:05 -0500

    updated i2c.js file to include checks for i2C destination availability

--- i2c.js
+++ i2c.js
@@ -12,37 +12,50 @@ var dome;
 var bodyservos;
 var hp;
 function i2CSend(ldp, coin, vu, maint, textcommand, dp, cbi, i2ccommand, i2cdevice, hpFront, hpTop, hpRear) {
-  scan();
-if (bodyleds == true) {
-  sleep(25).then(() => { BodyledSend(ldp, bodyLEDI2C ); });
-  sleep(50).then(() => { BodyledSend(coin, bodyLEDI2C ); });
-  sleep(75).then(() => { BodyledSend(vu, bodyLEDI2C ); });
-  sleep(100).then(() => { BodyledSend(maint, bodyLEDI2C ); });
-  sleep(125).then(() => { BodyledSend(dp, bodyLEDI2C ); });
-  sleep(150).then(() => { BodyledSend(cbi, bodyLEDI2C ); });
+  scan(); //used to check if the i2c destination is availabe.  If available the var is "true".  WIthout this check, the server crashes when it tries to send a command to an i2c device that isn't online.
+if (bodyleds == true) {     //used to check if the i2c destination is availabe
+  //put the commands for the Body LED Controller here
+  sleep(25).then(() => { i2CCommandSend(ldp, bodyLEDI2C ); });
+  sleep(50).then(() => { i2CCommandSend(coin, bodyLEDI2C ); });
+  sleep(75).then(() => { i2CCommandSend(vu, bodyLEDI2C ); });
+  sleep(100).then(() => { i2CCommandSend(maint, bodyLEDI2C ); });
+  sleep(125).then(() => { i2CCommandSend(dp, bodyLEDI2C ); });
+  sleep(150).then(() => { i2CCommandSend(cbi, bodyLEDI2C ); });
 
 };
 if (hp == true){
-  sleep(200).then(() => { BodyledSend(hpFront, domeHPI2C); });
-  sleep(225).then(() => { BodyledSend(hpTop, domeHPI2C); });
-  sleep(250).then(() => { BodyledSend(hpRear, domeHPI2C); });
+  //put the commands for the HP Controller here
+  sleep(200).then(() => { i2CCommandSend(hpFront, domeHPI2C); });
+  sleep(225).then(() => { i2CCommandSend(hpTop, domeHPI2C); });
+  sleep(250).then(() => { i2CCommandSend(hpRear, domeHPI2C); });
 
 }
 
-  sleep(175).then(() => { i2cCommandSend(i2ccommand, i2cdevice); });
+if (dome == true){
+//put commands for the Dome Servo and Lens Controller here
+}
+if (bodyservos == true){
+  //put commands for the Body Servos Controller here
+
+}
+if (stealth == true){
+  //put commands for the Stealth System here
+
+}
+  sleep(175).then(() => { i2cTextBoxCommandSend(i2ccommand, i2cdevice); });
 
   console.log("Complete");
 };
 
-function BodyledSend(led, i2cdestled) {
+function i2CCommandSend(led, i2cdestled) {
   const i2c1 = i2c.open(1, function (err) {
         if (err) {
           throw err;
         }
 
-        if (typeof (led) != 'undefined') {
+        if (typeof (led) != 'undefined') {  //checks to see if there is a command to send.  If Undefine(No command, then do nothing)
           var ledcommand = Buffer.from(led);
-          console.log('Command Recieved: ' + ledcommand);
+          // console.log('Command Recieved: ' + ledcommand);
           i2c1.i2cWrite(i2cdestled, ledcommand.length, ledcommand, function (err) {
             if (err) {
               throw err;
@@ -55,7 +68,7 @@ function BodyledSend(led, i2cdestled) {
       });
 };
 
-function i2cCommandSend(i2ctextcommand, device) {
+function i2cTextBoxCommandSend(i2ctextcommand, device) {
   const i2c1 = i2c.open(1, function (err) {
         if (err) {
           throw err;
