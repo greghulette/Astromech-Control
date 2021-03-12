3a86596fc941ce971473c821741d4615abdd0983
greghulette <greg.hulette@gmail.com>
2021-03-12 14:20:27 -0500

    updateing open/close

--- public/js/main-test.js
+++ public/js/main-test.js
@@ -221,16 +221,25 @@ function changeImageVerticalBarsKnightRider() {
   //   document.getElementById(u).src = "Images/blankcheckmark.png";
   //
   // }
-
+var dstatus = true;
 function openAllDoorsMS(i){
 
-  var dscommandstring = i
+
   console.log(dscommandstring);
+  if (dstatus == true;){
+      var dscommandstring = "d03"
+      dstatus = false;
   socket.emit('command', {
     doorservocommandstring: dscommandstring,
-
+}
   });
-
+  if (dstatus == fa;se;){
+      var dscommandstring = "d04"
+      dstatus = true;
+  socket.emit('command', {
+    doorservocommandstring: dscommandstring,
+  }
+  });
 
 }
 function commandNoOptionsKnightRider( y, t, u) {
