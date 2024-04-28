2298460f297715182c9bb3bb0e590a03ecf37e5a
greghulette <31554549+greghulette@users.noreply.github.com>
2024-04-27 20:41:31 -0400

    Update simple-cors-http-server.py

--- UPSPACK_V3-master/UPS_GUI_py/simple-cors-http-server.py
+++ UPSPACK_V3-master/UPS_GUI_py/simple-cors-http-server.py
@@ -20,7 +20,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         self.end_headers()
 
 host = sys.argv[1] if len(sys.argv) > 2 else '0.0.0.0'
-port = int(sys.argv[len(sys.argv)-1]) if len(sys.argv) > 1 else 8080
+port = int(sys.argv[len(sys.argv)-1]) if len(sys.argv) > 1 else 8000
 
 print("Listening on {}:{}".format(host, port))
 httpd = HTTPServer((host, port), CORSRequestHandler)
