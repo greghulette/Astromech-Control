634351b59b3649567e39ddb443989aa23de1d0d5
greghulette <31554549+greghulette@users.noreply.github.com>
2024-04-27 20:34:03 -0400

    Update simple-cors-http-server.py

--- UPSPACK_V3-master/UPS_GUI_py/simple-cors-http-server.py
+++ UPSPACK_V3-master/UPS_GUI_py/simple-cors-http-server.py
@@ -1,11 +1,30 @@
-#!/usr/bin/env python3
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+# Python http.server that sets Access-Control-Allow-Origin header.
+# https://gist.github.com/razor-x/9542707
+
+import os
 import sys
+import http.server
+import socketserver
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
+PORT = 8000
+
+class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
 
-if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+def server(port):
+    httpd = socketserver.TCPServer(('', port), HTTPRequestHandler)
+    return httpd
+
+if __name__ == "__main__":
+    port = PORT
+    httpd = server(port)
+    try:
+        os.chdir('build')
+        print("\nserving from build/ at localhost:" + str(port))
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\n...shutting down http server")
+        httpd.shutdown()
+        sys.exit()
