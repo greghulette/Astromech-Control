f583005c8c453547bde5e3bb46c6c24014d4fccd
greghulette <31554549+greghulette@users.noreply.github.com>
2024-04-28 23:08:39 -0400

    updates to autostart

---
+++ SetupPi.sh
@@ -0,0 +1,3 @@
+mkdir /home/pi/.config/lxsession
+mkdir /home/pi/.config/lxsession/LXDE-pi
+cp autostart /home/pi/.config/lxsession/LXDE-pi/
---
+++ UPSPACK_V3-master/UPS_GUI_py/starthttp.sh
@@ -0,0 +1,2 @@
+cd /home/pi/Astromech-Control/UPSPACK_V3-master/UPS_GUI_py/
+python3 /home/pi/Astromech-Control/UPSPACK_V3-master/UPS_GUI_py/simple-cors-http-server.py
